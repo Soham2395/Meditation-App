e4c4c25c0e5c890f7d43afb6dbdbc5b0202f492d
Soham2395 <soham2395.be22@chitkara.edu.in>
2024-04-19 18:10:43 +0530

    Responsiveness added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useRef, useEffect} from 'react';
 import './header.css';
 
 const nav__links=[
@@ -11,7 +11,7 @@ const nav__links=[
         display:'About'
     },
     {
-        path:'#services',
+        path:'#service',
         display:'Services'
     },
     {
@@ -25,18 +25,44 @@ const nav__links=[
 
 ]
 const Header = ({theme,toggleTheme}) => {
+    const headerRef=useRef(null);
+    const menuRef=useRef(null);
+    const headerFunc=()=>{
+        if(document.body.scrollTop>80||document.documentElement.scrollTop>80){
+            headerRef.current.classList.add('header__shrink')
+        }
+        else{
+            headerRef.current.classList.remove('header__shrink')
+        }
+    }
+    useEffect(()=>{
+        window.addEventListener('scroll',headerFunc)
+        return ()=>
+            window.removeEventListener('scroll',headerFunc)
+        },[]);
+
+    const handleClick=e=>{
+        e.preventDefault()
+        const targetAttr=e.target.getAttribute('href')
+        const location=document.querySelector(targetAttr).offsetTop;
+        window.scrollTo({
+            left:0,
+            top:location-80,
+        });
+    };
+    const toggleMenu=()=>menuRef.current.classList.toggle('menu__active')
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
         <div className="container">
             <div className="nav__wrapper">
                 <div className="logo">
                     <h2>Meditation App</h2>
                 </div>
-                <div className="navigation">
+                <div className="navigation" ref={menuRef} onClick={toggleMenu}>
                     <ul className="menu">
                         {nav__links.map((item,index)=>(
-                            <li className="menu__item">
-                                <a href={item.path} className="menu__link">{item.display}
+                            <li className="menu__item" key={index}>
+                                <a href={item.path} onClick={handleClick} className="menu__link">{item.display}
                                 </a>
                             </li>
                         ))}
@@ -45,11 +71,17 @@ const Header = ({theme,toggleTheme}) => {
                 <div className="light__mode">
                     <span onClick={toggleTheme}>
                         {
-                            theme==='light-theme'?<span><i class="ri-moon-line"></i>Dark</span>
-                            :<span><i class="ri-sun-line"></i>Light</span>
-                        }
+                            theme==='light-theme'?(
+                            <span><i class="ri-moon-line"></i>Dark</span>
+                            ):(
+                            <span><i class="ri-sun-line"></i>Light</span>
+                        )}
                     </span>
                 </div>
+
+                <span className="mobile__menu" onClick={toggleMenu}>
+                    <i class="ri-menu-line"></i>
+                </span>
             </div>
         </div>
     </header>
