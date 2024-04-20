79e4788064d4ccf369ceb06d6be0b86080bd866c
Soham2395 <soham2395.be22@chitkara.edu.in>
2024-04-20 12:58:50 +0530

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import Spinner from "react-spinkit";
 
 import Header from './components/Header/Header';
 import Hero from './components/UI/Hero';
@@ -13,25 +14,44 @@ import Newsletter from './components/UI/Newsletter';
 import Footer from './components/Footer/Footer';
 
 function App() {
-  const [theme,setTheme]=useState('')
-  const toggleTheme=()=>{
-    theme==='' ?setTheme('light-theme'):setTheme('')
+  const [theme, setTheme] = useState('');
+  const [isLoading, setIsLoading] = useState(true);
+
+  const toggleTheme = () => {
+    theme === '' ? setTheme('light-theme') : setTheme('');
   }
-  useEffect(()=>{
-    document.body.className=theme},[theme])
+
+  useEffect(() => {
+    document.body.className = theme;
+
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [theme]);
 
   return (
     <>
-    <Header theme={theme} toggleTheme={toggleTheme}/>
-    <Hero theme={theme}/>
-    <Counter/>
-    <Services/>
-    <About/>
-    <Team/>
-    <Blog/>
-    <Testimonial/>
-    <Newsletter/>
-    <Footer/>
+      {isLoading ? (
+        <div className="spinner-container">
+          <h1>Improve your focus</h1>
+          <Spinner name="ball-spin-fade-loader" color="#379cf6"/>
+        </div>
+      ) : (
+        <>
+          <Header theme={theme} toggleTheme={toggleTheme} />
+          <Hero theme={theme} />
+          <Counter />
+          <Services />
+          <About />
+          <Team />
+          <Blog />
+          <Testimonial />
+          <Newsletter />
+          <Footer />
+        </>
+      )}
     </>
   );
 }
--- src/components/UI/Hero.jsx
+++ src/components/UI/Hero.jsx
@@ -1,32 +1,68 @@
-import React from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import '../../styles/hero.css';
 import heroDarkImg from '../../images/hero-img.png';
 import lightImg from '../../images/light-hero-bg.jpg';
 
-const Hero = ({theme}) => {
+const Hero = ({ theme }) => {
+  const [highlightText, setHighlightText] = useState('');
+  const [currentTextIndex, setCurrentTextIndex] = useState(0);
+  const [highlightVisible, setHighlightVisible] = useState(true);
+
+  const texts = useMemo(() => ['regain your focus', 'relax your mind', 'relieve your stress'], []);
+
+  useEffect(() => {
+    const textLength = texts[currentTextIndex].length;
+
+    if (highlightVisible) {
+      const timeout = setTimeout(() => {
+        setHighlightText(texts[currentTextIndex].substring(0, highlightText.length + 1));
+      }, 200); // Adjust typing speed as needed
+
+      if (highlightText.length === textLength) {
+        setHighlightVisible(false);
+        setTimeout(() => {
+          setCurrentTextIndex((currentTextIndex + 1) % texts.length);
+          setHighlightVisible(true);
+        }, 1500); // Delay before removing text
+      }
+
+      return () => clearTimeout(timeout);
+    } else {
+      const timeout = setTimeout(() => {
+        setHighlightText(highlightText.substring(0, highlightText.length - 1));
+      }, 100); // Adjust deletion speed as needed
+
+      if (highlightText.length === 0) {
+        setHighlightVisible(true);
+      }
+
+      return () => clearTimeout(timeout);
+    }
+  }, [highlightText, highlightVisible, currentTextIndex, texts]);
+
   return (
     <section className="hero__section">
-        <div className="container">
-            <div className="hero__wrapper">
-                <div className="hero__content">
-                    <div>
-                        <h2>We are creating perfect</h2>
-                        <h2>space for meditation to</h2>
-                        <h2 className="highlight">improve your focus</h2>
-                    </div>
-                    <p className="description">Our app offers a variety of tools to help you reduce stress, improve focus, and enhance your overall well-being!</p>
-                    <div className="hero__btns">
-                        <button className="primary__btn">Get Started Now</button>
-                        <button className="secondary__btn">Discover More</button>
-                    </div>
-                </div>
-                <div className="hero__img">
-                    <img src={theme==='light-theme' ?lightImg:heroDarkImg} alt="hero-img"/>
-                </div>
+      <div className="container">
+        <div className="hero__wrapper">
+          <div className="hero__content">
+            <div>
+              <h2>We are creating perfect</h2>
+              <h2>space for meditation to</h2>
+              <h2 className="highlight">{highlightText}</h2>
             </div>
+            <p className="description">Our app offers a variety of tools to help you reduce stress, improve focus, and enhance your overall well-being!</p>
+            <div className="hero__btns">
+              <button className="primary__btn">Get Started Now</button>
+              <button className="secondary__btn">Discover More</button>
+            </div>
+          </div>
+          <div className="hero__img">
+            <img src={theme === 'light-theme' ? lightImg : heroDarkImg} alt="hero-img" />
+          </div>
         </div>
+      </div>
     </section>
   )
 }
 
-export default Hero
+export default Hero;
